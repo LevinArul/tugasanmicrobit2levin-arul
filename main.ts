fb4aab88fa62c7a2500b0d3bf85a856a7a80e32e
LevinArul <86990787+LevinArul@users.noreply.github.com>
2021-07-13 07:12:37 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,30 @@
+basic.showString("LEVIN ARUL 5 ARIF ")
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . . # . .
+    . # # # .
+    # # # # #
+    . # # # .
+    . . # . .
+    `)
+basic.showIcon(IconNames.Heart)
+basic.pause(1000)
+basic.forever(function () {
+	
+})
